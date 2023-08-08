cf951ea429dbaa38131dc0dd3f4d9bff370ac24f
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-09 00:06:52 +0530

    Added auth module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
