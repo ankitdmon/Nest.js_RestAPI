72dad258f9c7cafaa03dfbac4fd3c30a2f5cd917
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-09 00:18:51 +0530

    User schema Added.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schema/user.schema';
 
 @Module({
+  imports: [
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    MongooseModule.forFeature([{name: 'User', schema:UserSchema}])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
