d82ffa825a9d23bf609fd5ab3d807cdd520da0cf
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-09 00:57:35 +0530

    SignUp API using JWT token.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+        constructor(private authService: AuthService) {}
+      
+        @Post('/signup')
+        signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+          return this.authService.signUp(signUpDto);
+        }
+      
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt'; // Import JwtService
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(signUpDto): Promise<{ token: string }> {
+    const { name, email, password } = signUpDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = this.jwtService.sign({
+      id: user._id,
+    });
+
+    return { token };
+  }
+}
