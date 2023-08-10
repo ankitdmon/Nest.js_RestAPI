ba506c73d475fce136b1534373f027325a701317
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-10 16:33:26 +0530

    Bug Fixed!!

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
@@ -17,7 +21,11 @@ export class AuthService {
 
   async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
     const { name, email, password } = signUpDto;
+    const existingUser = await this.userModel.findOne({ email });
 
+    if (existingUser) {
+      throw new ConflictException('Email already in use');
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await this.userModel.create({
@@ -32,19 +40,19 @@ export class AuthService {
   }
 
   async login(loginDto: LoginDto): Promise<{ token: string }> {
-    const {email, password} = loginDto;
-    const user = await this.userModel.findOne({email});
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
 
-    if(!user){
-        throw new UnauthorizedException("Unauthorised email or password!!");
+    if (!user) {
+      throw new UnauthorizedException('Unauthorised email or password!!');
     }
 
-    const isPasswordMatched  =await bcrypt.compare(password, user.password);
-    if(!isPasswordMatched){
-        throw new UnauthorizedException('Invalid email or password!!');
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid email or password!!');
     }
-    const token  = this.jwtService.sign({id: user._id});
+    const token = this.jwtService.sign({ id: user._id });
 
-    return {token};
+    return { token };
   }
 }
