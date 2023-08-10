2b57b81feeb8920113c9fe360e5f8c84ddbecbd8
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-10 17:23:33 +0530

    Validations added.

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,9 +1,35 @@
-import { Category } from "../schemas/book.schema"
+import {
+  IsEmpty,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+import { Category } from '../schemas/book.schema';
+import { User } from 'src/auth/schema/users.auth';
 
 export class CreateBookDto {
-    readonly title: string
-    readonly description: string
-    readonly author: string
-    readonly price: number
-    readonly category: Category
+  @IsNotEmpty()
+  @IsString()
+  readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly price: number;
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'Please enter correct category.' })
+  readonly category: Category;
+
+  @IsEmpty({ message: 'You cannot pass user id' })
+  readonly user: User;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+app.useGlobalPipes(new ValidationPipe());
+
   const PORT = process.env.PORT || 5001;
   await app.listen(PORT);
   console.log(`Server is running on PORT: ${PORT}`);
