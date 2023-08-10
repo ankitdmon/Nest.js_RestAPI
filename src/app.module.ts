d19ef01ec13743bb8e68accb77d25a93a5297138
Ankit-flutch <93345314+ankitdmon@users.noreply.github.com>
2023-08-10 15:38:55 +0530

    SignUp Using JWT.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     BookModule,
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
